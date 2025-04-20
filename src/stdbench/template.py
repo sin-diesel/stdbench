de76d51aaa098ab9d717b59dbc800a0122670ed3
Stanislav Sidelnikov <sidelnikov.si.001@gmail.com>
2025-04-20 14:20:00 +0300

    Move generation logic to jinja2

--- src/stdbench/template.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-
-# class Template:
-#    def __init__(self, template_name: str) -> None:
-#        self._template_name = template_name
-#
-#    def set_configs(self, configs: list[Config]) -> None:
-#        self._configs = configs
--- src/stdbench/utils.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-from pathlib import Path
-
-repo_root = Path().parent.parent
