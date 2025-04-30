c1f24815d21090cf4019a0832bcfdfc03012361c
Stanislav Sidelnikov <sidelnikov.si.001@gmail.com>
2025-04-30 17:09:00 +0300

    feat: Implement benchmark generation via configuration file (#16)
    * Add BenchGenerator using yaml description
    
    * Add benchmark generation via configuration file, add ctest to CI

--- src/stdbench/benchmark.py
+++ src/stdbench/benchmark.py
@@ -1,16 +1,41 @@
 import os
-from jinja2 import Environment
+import yaml
+from itertools import product
+from jinja2 import Environment, Template, FileSystemLoader
 from pathlib import Path
 
 
 class Benchmark:
-    def __init__(self, *, name: str, env: Environment, template: str, output_dir: Path) -> None:
-        self._env = env
-        self._template = env.get_template(template)
-        self._output_dir = output_dir
-        self._name = name
+    def __init__(self, params) -> None:
+        self._params = params
 
-    def generate(self, **params: str) -> None:
-        os.makedirs(self._output_dir, exist_ok=True)
-        benchmark_path = self._output_dir / f"{self._name}.cpp"
-        benchmark_path.write_text(self._template.render(name=self._name, **params))
+    def generate(self, template: Template, output_dir: Path) -> None:
+        os.makedirs(output_dir, exist_ok=True)
+        benchmark_path = output_dir / f"{self._params['name']}.cpp"
+        benchmark_path.write_text(template.render(**self._params))
+
+
+class BenchGenerator:
+    def __init__(self, config_path: Path, templates_path: Path) -> None:
+        self._config_path = config_path
+        self._templates_path = templates_path
+
+    def generate(self) -> None:
+        env = Environment(loader=FileSystemLoader(self._templates_path))
+
+        with open(self._config_path, "r") as file:
+            contents = yaml.safe_load(file)
+
+        template = env.get_template(contents["template"])
+        del contents["template"]
+
+        configs = []
+        for key, value in contents.items():
+            configs.append([{key: v} for v in value])
+
+        for params in product(*configs):
+            params_dict = {}
+            for field in params:
+                params_dict.update(field)
+            benchmark = Benchmark(params_dict)
+            benchmark.generate(template=template, output_dir=Path("benchmarks"))
--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,27 +1,10 @@
-from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
-from stdbench.benchmark import Benchmark
 
-def test_benchmarks():
-    env = Environment(loader=FileSystemLoader("templates"))
-    copy = Benchmark(name="copy", env=env, template="input_range_output_iterator.jinja", output_dir=Path("benchmarks"))
-    copy.generate(src_container="std::vector", T="int", size=1000, dst_container="std::vector")
+from stdbench.benchmark import Benchmark, BenchGenerator
 
-    for_each = Benchmark(name="for_each", env=env, template="input_range.jinja", output_dir=Path("benchmarks"))
-    for_each.generate(src_container="std::vector", T="int", size=1000, func="[](int &n) { n++; }")
+_repo_root = Path(__file__).parent.parent
 
-    for_each_n = Benchmark(name="for_each_n", env=env, template="input_range_value.jinja", output_dir=Path("benchmarks"))
-    for_each_n.generate(src_container="std::vector", T="int", size=1000, last=3, func="[](auto& n) { n *= 2; }")
-
-    all_of = Benchmark(name="all_of", env=env, template="input_range.jinja", output_dir=Path("benchmarks"))
-    all_of.generate(src_container="std::vector", T="int", size=1000, func="[](int i) { return i % 2 == 0; }")
-
-    any_of = Benchmark(name="any_of", env=env, template="input_range.jinja", output_dir=Path("benchmarks"))
-    any_of.generate(src_container="std::vector", T="int", size=1000, func="[](int i) { return i % 2 == 0; }")
-
-    none_of = Benchmark(name="none_of", env=env, template="input_range.jinja", output_dir=Path("benchmarks"))
-    none_of.generate(src_container="std::vector", T="int", size=1000, func="[](int i) { return i % 2 == 0; }")
-
-    find_if = Benchmark(name="find_if", env=env, template="input_range.jinja", output_dir=Path("benchmarks"))
-    find_if.generate(src_container="std::vector", T="int", size=1000, func="[](int i) { return i % 2 == 0; }")
-
+def test_benchmarks_config():
+    config_path = _repo_root / "tests" / "config.yaml"
+    benchmarks = BenchGenerator(config_path, templates_path=_repo_root / "templates")
+    benchmarks.generate()
