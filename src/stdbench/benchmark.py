5bd2707ad8607b380dbfeb6345033e7295bfb604
Stanislav Sidelnikov <sidelnikov.si.001@gmail.com>
2025-05-14 16:04:52 +0300

    Add compiler environment configuration, generate benchmarks in build directory (#22)
    * Experimental support for specifying compiler and compiler opts in config.yaml
    
    * Generate benchmarks in build, document the changes

--- src/stdbench/benchmark.py
+++ src/stdbench/benchmark.py
@@ -1,8 +1,16 @@
 import os
 import yaml
+import shutil
+
 from itertools import product
 from jinja2 import Environment, Template, FileSystemLoader
 from pathlib import Path
+
+
+CMAKE_ENV_VARS_TEMPLATE = """
+set(COMPILER {})
+set(COMPILER_OPTS {})
+"""
 
 
 class Benchmark:
@@ -10,8 +18,6 @@ class Benchmark:
         self._params = params
 
     def generate(self, template: Template, output_dir: Path) -> None:
-        os.makedirs(output_dir, exist_ok=True)
-
         forbidden_characters = ["{", "}", "[", "]", "(", ")", ";", ":", "="]
         bench_name = ("_".join(self._params.values())).replace(" ", "_")
         bench_name = bench_name.translate({ord(char): "_" for char in forbidden_characters})
@@ -23,26 +29,42 @@ class Benchmark:
 
 
 class BenchGenerator:
-    def __init__(self, config_path: Path, templates_path: Path) -> None:
+    def __init__(self, config_path: Path, templates_path: Path, output_dir=Path("build") / "benchmarks") -> None:
         self._config_path = config_path
         self._templates_path = templates_path
+        self._output_dir = output_dir
+
+        if self._output_dir.exists():
+            shutil.rmtree(self._output_dir)
+        os.mkdir(self._output_dir)
+
+    def _generate_env(self, config: dict[str, str | list[str]]) -> None:
+        compilers = " ".join(config["compiler"])
+        compiler_opts = " ".join(config["compiler_opts"])
+        env_vars_path = self._output_dir / "cmake_env_vars.cmake"
+
+        env_vars_path.write_text(CMAKE_ENV_VARS_TEMPLATE.format(compilers, compiler_opts))
+        del config["compiler"]
+        del config["compiler_opts"]
 
     def generate(self) -> None:
         env = Environment(loader=FileSystemLoader(self._templates_path))
 
         with open(self._config_path, "r") as file:
-            contents = yaml.safe_load(file)
+            config = yaml.safe_load(file)
 
-        template = env.get_template(contents["template"])
-        del contents["template"]
+        template = env.get_template(config["template"])
+        del config["template"]
 
-        configs = []
-        for key, value in contents.items():
-            configs.append([{key: v} for v in value])
+        self._generate_env(config)
 
-        for params in product(*configs):
+        subconfigs = []
+        for key, value in config.items():
+            subconfigs.append([{key: v} for v in value])
+
+        for params in product(*subconfigs):
             params_dict = {}
             for field in params:
                 params_dict.update(field)
             benchmark = Benchmark(params_dict)
-            benchmark.generate(template=template, output_dir=Path("benchmarks"))
+            benchmark.generate(template=template, output_dir=self._output_dir)
