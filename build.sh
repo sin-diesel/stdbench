170e05353f2d5e4f061fc45a0a1d85d4ce68da20
Stanislav Sidelnikov <stanislav.sidelnikov@yadro.com>
2025-05-19 14:06:11 +0300

    Update CI and docs

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
 COMPILERS="g++ clang++-18"
 
 for COMPILER in $COMPILERS; do
-  cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=build/conan_toolchain.cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release -B build/$COMPILER
+  cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$SCRIPT_DIR/build/conan_toolchain.cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release -B $SCRIPT_DIR/build/$COMPILER
 done
 
 for COMPILER in $COMPILERS; do
