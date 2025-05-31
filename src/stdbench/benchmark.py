1f6cf5cd3a8c1978df557114c3db4b075e84f4b2
Stanislav Sidelnikov <sidelnikov.si.001@gmail.com>
2025-05-31 20:15:23 +0300

    Update

--- src/stdbench/benchmark.py
+++ src/stdbench/benchmark.py
@@ -8,7 +8,7 @@ from jinja2 import Environment, Template, FileSystemLoader
 from dataclasses import dataclass
 from pathlib import Path
 
-from stdbench.config import Config
+from stdbench.config import Config, ProcessedConfig
 
 
 CMAKE_ENV_VARS_TEMPLATE = """
@@ -34,7 +34,15 @@ class Measurement:
 
 
 class Benchmark:
-    def __init__(self, params) -> None:
+    def __init__(
+            self,
+            *,
+            template: Template,
+            output_dir: Path,
+            params: ProcessedConfig
+        ) -> None:
+        self._template = template
+        self._output_dir = output_dir
         self._params = params
 
     def generate(self, template: Template, output_dir: Path) -> None:
@@ -49,28 +57,12 @@ class Benchmark:
 
 
 class BenchGenerator:
-    def __init__(self, config_path: Path, templates_path: Path, output_dir=Path("build") / "benchmarks") -> None:
+    def __init__(self, config_path: Path, output_dir: Path, templates_path: Path) -> None:
         self._config = Config(config_path)
-
-        self._templates_path = templates_path
         self._output_dir = output_dir
-        self._subconfigs = []
-
-        if self._output_dir.exists():
-            shutil.rmtree(self._output_dir)
-        os.mkdir(self._output_dir)
+        self._templates_path = templates_path
 
         self._env = Environment(loader=FileSystemLoader(self._templates_path))
-
-        with open(self._config_path, "r") as file:
-            self._config = yaml.safe_load(file)
-
-    def _generate_env(self, config: dict[str, str | list[str]]) -> None:
-        compiler_opts = " ".join(config["compiler_opts"])
-        env_vars_path = self._output_dir / "compiler_opts.cmake"
-
-        env_vars_path.write_text(CMAKE_ENV_VARS_TEMPLATE.format(compiler_opts))
-        del config["compiler_opts"]
 
     def benchmark_names(self) -> list[str]:
         return self._config["name"]
@@ -79,36 +71,13 @@ class BenchGenerator:
         return self._benchmark_configs
 
     def generate(self) -> list[Benchmark]:
-        benchmarks: list[Benchmark] = []
-        template = self._env.get_template(self._config["template"])
-        del self._config["template"]
-
-        self._generate_env(self._config)
-
-        subconfigs = []
-        for key, value in self._config.items():
-            subconfigs.append([{key: v} for v in value])
-
-        benchmark_config = copy.deepcopy(self._config)
-        benchmark_configs = []
-        del benchmark_config["size"]
-
-        for key, value in benchmark_config.items():
-            benchmark_configs.append([{key: v} for v in value])
-
-        self._benchmark_configs = list(product(*benchmark_configs))
-
-        self._subconfigs = list(product(*subconfigs))
-
         transposed_bench_configs = self._config.benchmark_config(transposed=True)
         cartesian_product = list(product(*transposed_bench_configs))
 
+        benchmarks: list[Benchmark] = []
         for config in cartesian_product:
-            
-            for field in params:
-                params_dict.update(field)
-            benchmark = Benchmark(params_dict)
-            benchmark.generate(template=template, output_dir=self._output_dir)
+            params = {value for value in config}
+            benchmark = Benchmark(template=self_config.template, output_dir=self._output_dir, params=params)
             benchmarks.append(benchmark)
 
         return benchmarks
