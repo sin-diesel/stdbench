a86f5a448c2c47e0d7a6c621a3c1d03d58598d69
Stanislav Sidelnikov <sidelnikov.si.001@gmail.com>
2025-06-02 17:04:51 +0300

    Add parallel execution to build.sh and test.sh

--- src/stdbench/benchmark.py
+++ src/stdbench/benchmark.py
@@ -11,11 +11,6 @@ from pathlib import Path
 from stdbench.config import Config, NormalizedConfig
 
 
-CMAKE_ENV_VARS_TEMPLATE = """
-set(COMPILER_OPTS {})
-"""
-
-
 @dataclass(kw_only=True)
 class Measurement:
     name: str
@@ -56,6 +51,21 @@ class Benchmark:
         benchmark_path.write_text(self._template.render(**self._params))
 
 
+class CMakeHints:
+    def __init__(self, path: Path) -> None:
+        self._hints: list[tuple[str, str]] = []
+        self._path = path
+
+    def add(self, var: str, value: str) -> None:
+        if not isinstance(var, str) or not isinstance(value, list):
+            raise ValueError("Incorrect config, all keys must be str, all values must be lists")
+        self._hints.append((var, value))
+
+    def generate(self) -> None:
+        hints_text = "".join([f"SET(\"{hint[0]}\" {' '.join(hint[1])})\n" for hint in self._hints])
+        self._path.write_text(hints_text)
+
+
 class BenchGenerator:
     def __init__(self, *, config_path: Path, output_dir: Path, templates_path: Path) -> None:
         self._config = Config(config_path)
@@ -69,6 +79,11 @@ class BenchGenerator:
         self._env = Environment(loader=FileSystemLoader(self._templates_path))
         self._template = self._env.get_template(self._config.template)
 
+    def _generate_cmake_hints(self) -> None:
+        cmake_hints = CMakeHints(path=self._output_dir / "hints.cmake")
+        for var, value in self._config.environment_config().items():
+            cmake_hints.add(var=var, value=value)
+        cmake_hints.generate()
 
     def generate(self) -> list[Benchmark]:
         transposed_bench_configs = self._config.benchmark_config(transposed=True)
@@ -80,5 +95,7 @@ class BenchGenerator:
             benchmark = Benchmark(template=self._template, output_dir=self._output_dir, params=params)
             benchmark.generate()
             benchmarks.append(benchmark)
+
+        self._generate_cmake_hints()
 
         return benchmarks
